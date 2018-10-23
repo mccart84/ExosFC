2b9159798be608f6d982fa8afd25290efeacc9c7
Ryan McCarty <ryan.mccarty90@hotmail.com>
2018-10-22 20:27:17 -0400

    Adding CSS and Files

--- ExosFC/ClientApp/src/App.js
+++ ExosFC/ClientApp/src/App.js
@@ -2,13 +2,21 @@
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import Members from './components/Members';
+import RaidTeams from './components/RaidTeams';
+import JobRequests from './components/JobRequests';
+import Events from './components/Events';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+    <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/members' component={Members} />
+        <Route path='/raidteams' component={RaidTeams} />
+        <Route path='/jobrequests' component={JobRequests} />
+        <Route path='/events' component={Events} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    </Layout>
 );
--- ExosFC/ClientApp/src/components/NavMenu.js
+++ ExosFC/ClientApp/src/components/NavMenu.js
@@ -5,31 +5,51 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export default props => (
-  <Navbar inverse fixedTop fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to={'/'}>ExosFC</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
+    <Navbar inverse fixedTop fluid collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <Link to={'/'}>ExosFC</Link>
+            </Navbar.Brand>
+        <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to={'/'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
-      </Nav>
+        <Nav>
+            <LinkContainer to={'/'} exact>
+                <NavItem>
+                    <Glyphicon glyph='home' /> Home
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/counter'}>
+                <NavItem>
+                    <Glyphicon glyph='education' /> Counter
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/fetchdata'}>
+                <NavItem>
+                    <Glyphicon glyph='th-list' /> Fetch data
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/members'}>
+                <NavItem>
+                    <Glyphicon glyph='user' /> Members
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/raidTeams'}>
+                <NavItem>
+                    <Glyphicon glyph='king' /> Raid Teams
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/events'}>
+                <NavItem>
+                    <Glyphicon glyph='bookmark' /> Events
+                </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/jobRequests'}>
+                <NavItem>
+                   <Glyphicon glyph='grain' /> Job Requests
+                </NavItem>
+            </LinkContainer>
+        </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
